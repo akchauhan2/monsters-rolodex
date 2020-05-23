5bd41cec1acdc6db1f6a85af1777583034b3627a
= <vijayid_94@hotmail.com>
2020-05-23 19:24:46 +0530

    Updates

---
+++ precache-manifest.fd4b28c4d059b93c079e491e241e922c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f691e23c96c32c9f386cd1b4b12763",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "668ea7830240c51fa0c2",
+    "url": "/monsters-rolodex/static/css/main.5cc2de04.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668ea7830240c51fa0c2",
+    "url": "/monsters-rolodex/static/js/main.6e93a895.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
